de8b8172fb885eb7d78cdd7149a8eeecdaffe7ac
Akhil Datla <66145155+akhil-datla@users.noreply.github.com>
2022-12-05 23:54:30 -0800

    Push practice

--- project_videos/sa_practice_3.py
+++ project_videos/sa_practice_3.py
@@ -4,28 +4,28 @@ from manim import UP, ThreeDScene, Tex, DOWN, Write, SurroundingRectangle, Creat
 class SA_Practice_Problem(ThreeDScene):
     def construct(self):
         step1 = (
-            Tex(r"$S = \int_{0}^{2}2\pi x^{3} \sqrt{1+(\frac{dy}{dx})^{2}}dx$")
+            Tex(r"$y=x^2 \rightarrow x=\sqrt{y}$")
             .move_to(UP)
             .shift(UP * 2)
         )
-        step2 = Tex(r"$S = \int_{0}^{2}2\pi x^{3} \sqrt{1+(3x^{2})^{2}}dx$").next_to(
+        step2 = Tex(r"$S=\int_{0}^{3}2\pi\sqrt{y}\sqrt{1+(\frac{1}{2\sqrt{y}})^2}dy$").next_to(
             step1, DOWN, buff=0.2
         )
-        step3 = Tex(r"$S = 2\pi\int_{0}^{2}x^{3}(1+9x^{4})^{1/2}dx$").next_to(
+        step3 = Tex(r"$S=\pi\int_{0}^{3}\sqrt{4y+1}dy$").next_to(
             step2, DOWN, buff=0.2
         )
 
-        step4 = Tex(r"$u=1+9x^4, du=36x^{3}dx$").next_to(step3, DOWN, buff=0.2)
+        step4 = Tex(r"$u=4y+1$").next_to(step3, DOWN, buff=0.2)
 
-        step5 = Tex(r"$S=\frac{2}{36}\pi \int_{1}^{145}u^{1/2}du$").next_to(
+        step5 = Tex(r"$S=\frac{\pi}{4}\int_{1}^{13}u^{1/2}du$").next_to(
             step4, DOWN, buff=0.2
         )
 
         step6 = Tex(
-            r"$S=\frac{2\pi}{36}\left[\ \frac{2}{3}u^{3/2} \right]_1^{145}$"
+            r"$\frac{\pi}{6}[(13)^{3/2}-1]$"
         ).next_to(step5, DOWN, buff=0.2)
 
-        step7 = Tex(r"$S \approx 203 \ units^{2}$").next_to(step6, DOWN, buff=0.2)
+        step7 = Tex(r"$S \approx 24 \ units^{2}$").next_to(step6, DOWN, buff=0.2)
 
         box = SurroundingRectangle(step7, buff=0.1)
 
